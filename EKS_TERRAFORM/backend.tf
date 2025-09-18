7a71642da7992d4d039c99a3d45b033c41d8d315
VG <gvanitha980@gmail.com>
2025-09-18 11:04:22 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "vani-hotstar1" # Replace with your actual S3 bucket name
+    bucket = "hotstar1" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
